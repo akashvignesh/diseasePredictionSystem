ae2d275d9d2ce7c2db1eefc0427fd5fd72e386fd
Akash Sureshkumar <s3akash@gmail.com>
2024-12-09 16:08:59 -0500

    Adding csv and Intialized DB

---
+++ normalization.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlite3
+from sqlite3 import Error
+
+data = pd.read_csv("diabetes.csv")
+def create_connection(db_file, delete_db=False):
+    import os
+    if delete_db and os.path.exists(db_file):
+        os.remove(db_file)
+
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        conn.execute("PRAGMA foreign_keys = 1")
+    except Error as e:
+        print(e)
+
+    return conn
+
+create_connection("normalized.db")
